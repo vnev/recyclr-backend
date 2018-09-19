34d5c9a777517f3088e4b24d6d5a9839ea8c39bf
Zachary <zachary.t.rich@gmail.com>
2018-09-19 12:37:31 -0400

    added two more method stubs for updating/deleting

--- handlers/User.go
+++ handlers/User.go
@@ -11,8 +11,9 @@ import (
 
 // User : basic user schema
 type User struct {
-	ID   string
-	Name string
+	ID    string `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
 }
 
 // GetUsers : function to return a user from the database
@@ -46,4 +47,37 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	user.ID = strconv.Itoa(rand.Intn(1000000)) // Mock ID - not safe
 	users = append(users, user)
 	json.NewEncoder(w).Encode(user)
+}
+
+// UpdateUser : function to update a user in the database
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	var users []User // TODO: actually get this to read in users from the DB
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, user := range users {
+		if user.ID == params["id"] {
+			users = append(users[:index], users[index+1:]...)
+			var newUser User
+			_ = json.NewDecoder(r.Body).Decode(&newUser)
+			newUser.ID = params["id"]
+			users = append(users, newUser)
+			json.NewEncoder(w).Encode(newUser)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(users)
+}
+
+// DeleteUser : function to delete a user from the database
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	var users []User // TODO: actually get this to read in users from the DB
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, user := range users {
+		if user.ID == params["id"] {
+			users = append(users[:index], users[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(users)
 }
