51a387c26ca993f0ba3d197921f5a670021cf3da
Vedant Nevetia <nevetia.vedant@gmail.com>
2018-09-30 14:41:45 -0400

    wow

---
+++ handlers/Listing.go
@@ -0,0 +1,14 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+// Listing : basic listing schema, mimicks DB
+type Listing struct {
+	
+}
