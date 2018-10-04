d166628c4f79a60ec9937e8bc829c9498a795249
Zachary <zachary.t.rich@gmail.com>
2018-10-04 00:44:40 -0400

    fixed not getting id from route

--- handlers/User.go
+++ handlers/User.go
@@ -83,7 +83,8 @@ func UpdateUser(w http.ResponseWriter, r *http.Request) {
 
 	var user User
 	_ = json.NewDecoder(r.Body).Decode(&user)
-	if user.ID == 0 {
+	userID, err := strconv.Atoi(params["id"])
+	if userID == nil {
 		fmt.Println("Bad request 1")
 		http.Error(w, "No user ID found", http.StatusBadRequest)
 		return
