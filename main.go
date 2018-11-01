a285138c7aaaafd27571eb760aa123f7ac8deb44
Vedant Nevetia <nevetia.vedant@gmail.com>
2018-11-01 19:13:17 -0400

    woops wrong pointer

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 func loggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("%s %s\n", r.Method, r.URL)
-		(*w).Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		next.ServeHTTP(w, r)
 	})
 }
