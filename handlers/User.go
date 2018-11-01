5c3b7b910c0d8ecb71f76e809d4c03650273a58a
Vedant Nevetia <nevetia.vedant@gmail.com>
2018-10-31 23:17:59 -0400

    floating point divisions...

--- handlers/User.go
+++ handlers/User.go
@@ -477,7 +477,7 @@ func DeductUserPoints(w http.ResponseWriter, r *http.Request) {
 	var attr attributes
 	_ = json.NewDecoder(r.Body).Decode(&attr)
 
-	sqlStatement := `UPDATE Listings SET price=price-(price*($1/100)) WHERE listing_id=$2`
+	sqlStatement := `UPDATE Listings SET price=price-(price*($1/100.0)) WHERE listing_id=$2`
 	row, err := db.DBconn.Exec(sqlStatement, attr.Percentage, listingID)
 	if err != nil {
 		fmt.Println(err.Error())
